a7c2f615c580cf5ba31ae7241ce37c477fe1de1f
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-10-16 01:17:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// write the function to determine if the given string is palindrome.
+
+
+const string = "racecar"
+function palindrome(str){
+  var str = str.toLowerCase().replace()
+for (let i =0; i<str.length; i++){
+  if (str[i] !== str[str.length -1 -i]){
+return false
+  }
+  
+}return true
+}
+console.log(palindrome(string))
+
+
+
+
+
+
