da81ca3522905ee3f182fe50b218df0fc5b9b463
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-10-16 22:00:57 +0000

    option added

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-
 // write the function to determine if the given string is palindrome.
 
-    // const string = "racecar"
-
-    // function palindrome(str){
-    //       var str = str.toLowerCase().replace()
-    //     for (let i =0; i<str.length; i++){
-    //       if (str[i] !== str[str.length -1 -i ]){
-    //     return false
-    //       }
-          
-    //     }return true
-    // }
-    //     console.log(palindrome(string))
-
-// Another option
-
-function palindrome(str) {
-  var reg =/[\w_]/g;
- var smallstr = str.toLowerCase().replace(reg, "")
-  var reversed = smallstr.split(" ").reverse().join(" ")
-  if (reversed === smallstr)
-    return true;
-
-    return false;
-
+// Option 1
+const string = "racecar"
+function isPalindrome(str) {
+str = str.replace(/\W/g,"").toLowerCase();
+return (str == str.split("").reverse().join(""));
 }
- console.log(palindrome("eye"));
+ console.log(isPalindrome(string));
+
+
+
+// Option 2
+function palindrome(str){
+        var str = str.toLowerCase().replace()
+        for (let i =0; i<str.length; i++){
+          if (str[i] !== str[str.length -1 -i ]){
+        return false
+          }
+          
+        }return true
+}
+        console.log(palindrome(string))
+
 
 
 
