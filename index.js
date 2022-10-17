7bfcfae59f3dbeb454ff922e9fb49dfc6e2c526a
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-10-17 19:00:00 +0000

    option 3

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 // write the function to determine if the given string is palindrome.
 
 // Option 1
-const string = "racecar"
-function isPalindrome(str) {
-str = str.replace(/\W/g,"").toLowerCase();
-return (str == str.split("").reverse().join(""));
-}
- console.log(isPalindrome(string));
+// const string = "acecar";
+// //const len = string.length;
+// function isPalindrome(str) {
+// str = str.replace(/\W/g,"").toLowerCase();
+// return (str == str.split("").reverse().join(""));
+// }
+//  console.log(isPalindrome(string));
 
 
 
-// Option 2
-function palindrome(str){
-        var str = str.toLowerCase().replace()
-        for (let i =0; i<str.length; i++){
-          if (str[i] !== str[str.length -1 -i ]){
-        return false
-          }
+// // Option 2
+// function palindrome(str){
+//         var str = str.toLowerCase().replace()
+//         for (let i =0; i< string.length; i++){
+//           if (str[i] !== str[string.length -1 -i ]){
+//         return false
+//           }
           
-        }return true
-}
-        console.log(palindrome(string))
+//         }return true
+// }
+//         console.log(palindrome(string))
 
+
+// Option 3
+function checkPalindrome(string){
+    string = string.replace(/\W/g,"").toLowerCase();
+    const len =string.length;
+    for (let i=0; i < len; i++){
+        if(string[i]!== string[len-1-i]){
+            return "not palindrome";
+        }
+    } return "palindrome"
+}
+const string = prompt("Enter");
+const value = checkPalindrome(string);
+
+console.log(value);
+
+//Array Object determination 
+
+var colors = ["blue", "black", "red"];
+
+var cars = colors.map(color => color.toUpperCase().slice(0));
+console.log(cars)
 
 
 
